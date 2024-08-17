190437856f6b6f951fa80dea87a07bce7d3ad358
mikishlep <gleb.tseilonsky@yandex.ru>
2024-08-17 08:02:53 +0500

    first commit

---
+++ static/js/tooltipka.js
@@ -0,0 +1,10 @@
+tippy('#lang', {
+    trigger: 'click',
+    theme: 'light',
+    content: 
+    `<div>
+    <button id="tooltipButton" class="russian">Русский</button> / <button id="tooltipButton" class="english">English</button>
+    </div>`,
+    allowHTML: true,
+    interactive: true
+});
